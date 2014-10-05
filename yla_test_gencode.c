5d63dec0d30a72d0db8cce8e3110416d261e5c9e
Sergey Borisov <sergeybor@gmail.com>
2014-10-05 19:37:02 +0000

    Add functions for generating code. Usable for testing.

---
+++ yla_test_gencode.c
@@ -0,0 +1,36 @@
+/*
+    Generate code for test Virtual Machine
+
+    This file is part of YLA VM (Yet another Language for Academic purpose: Virtual Machine).
+
+    YLA VM is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "yla_test_gencode.h"
+#include "yla_type.h"
+
+void put_commd(yla_cop_type** prg, yla_cop_type value)
+{
+	**prg = value;
+	(*prg)++;
+}
+
+void put_value(yla_cop_type** prg, yla_int_type value)
+{
+	int i;
+	unsigned char* ptr = (unsigned char*)&value;
+	for (i=0; i<sizeof(yla_int_type); i++)
+		put_commd(prg, ptr[i]);
+}
