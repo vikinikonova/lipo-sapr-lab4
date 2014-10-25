6c4ba69e47b6c29692653beac14bcfe03e266382
Sergei Borisov <bsi@kcup.tusur.ru>
2014-10-25 19:08:17 +0700

    Add simple test to LOAD command. VM don't work.

--- yla_vm_vartable_tests.c
+++ yla_vm_vartable_tests.c
@@ -55,7 +55,72 @@ static int test_zero_table()
     return 0;
 }
 
+static int test_table_ok_internal(yla_int_type val)
+{
+    yla_cop_type prg[HEADER_SIZE + 1];
+    yla_cop_type *ptr = prg;
+    yla_int_type result = -1;
+
+    put_header(&ptr, 0, 1, 1);
+    put_commd(&ptr, CHALT);
+
+    yla_vm vm;
+
+    yla_vm_init(&vm, prg, HEADER_SIZE + 1);
+
+    YLATEST_ASSERT_TRUE(yla_vm_set_var(&vm, 0, val), "set 0 for zero var table");
+    YLATEST_ASSERT_TRUE(yla_vm_get_var(&vm, 0, &result), "set 0 for zero var table");
+    YLATEST_ASSERT_TRUE(result == val, "var changed after load");
+
+    yla_vm_do_command(&vm);
+    yla_vm_done(&vm);
+
+    return 0;
+}
+
+static int test_table_ok()
+{
+    test_table_ok_internal(10);
+    test_table_ok_internal(7432);
+}
+
+static int test_load_ok()
+{
+    yla_int_type val = 10;
+
+    yla_cop_type prg[HEADER_SIZE + 4];
+    yla_cop_type *ptr = prg;
+
+    yla_int_type result = -1;
+    yla_int_type buf[100];
+
+    put_header(&ptr, 1, 1, 4);
+    put_commd(&ptr, CLOAD);
+    put_value(&ptr, 0x0);
+    put_commd(&ptr, CHALT);
+
+    yla_vm vm;
+
+    yla_vm_init(&vm, prg, HEADER_SIZE + 4);
+
+    YLATEST_ASSERT_TRUE(yla_vm_set_var(&vm, 0, val), "set 0 for non-zero var table");
+
+    YLATEST_ASSERT_TRUE(yla_vm_do_command(&vm) == 1, "LOAD command performing error");
+
+    YLATEST_ASSERT_TRUE(yla_vm_stack_trace(&vm, buf, 1) == 0, "stack must have 1 value");
+    YLATEST_ASSERT_TRUE(buf[0] == val, "incorrect value in stack after load var");
+    YLATEST_ASSERT_TRUE(yla_vm_get_var(&vm, 0, &result), "get value from VM must work OK");
+    YLATEST_ASSERT_TRUE(result == val, "var changed after load");
+
+    yla_vm_do_command(&vm);
+    yla_vm_done(&vm);
+
+    return 0;
+}
+
 YLATEST_BEGIN(yla_vm_vartable_tests)
   YLATEST_ADD_TEST_CASE(test_zero_vm)
   YLATEST_ADD_TEST_CASE(test_zero_table)
+  YLATEST_ADD_TEST_CASE(test_table_ok)
+  YLATEST_ADD_TEST_CASE(test_load_ok)
 YLATEST_END
