f247f5c926629cb26fe9ca907104667a77f38096
5832_nvi <5832_nvi@kcup.net>
2017-01-28 09:57:36 +0000

    Add codes for operations for problem-oriented assembler

--- yla_cop.h
+++ yla_cop.h
@@ -30,6 +30,12 @@ codes of operations
 #define CDUP	0x02
 #define CDEEP	0x03
 #define CSTK	0x04
+#define CGDUP	0x0a
+#define CGDEEP	0x0b
+#define CGSTK	0x0c
+
+#define CTEST	0x05
+#define CCMP	0x06
 
 #define CLOAD	0x08
 #define CSAVE	0x09
@@ -40,12 +46,20 @@ codes of operations
 #define CDIV	0x13
 #define CNEG	0x14
 
-#define CJMP    0x20
-#define CJZ     0x21
-#define CJNZ    0x22
+#define CITOA	0x15
 
-#define CIN     0x30
-#define CINS    0x31
+#define CJMP	0x20
+#define CJZ		0x21
+#define CJNZ	0x22
+#define CJE		0x23
+#define CJNE	0x24
+
+#define CAL		0x40
+#define CALO	0x41
+#define CRET	0x42
+
+#define CIN		0x30
+#define CINS	0x31
 #define COUT	0x32
 
 #define CHALT	0xff
